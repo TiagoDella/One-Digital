c70af466c6850ea946ac82e812a359d20d54514c
TiagoDella <dellatiago81@gmail.com>
2024-04-12 12:57:27 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        lexend: ["Lexend Deca", "sans-serif"],
+      },
+    },
+  },
+  plugins: [],
+};
+// tailwind.config.js
+// tailwind.config.js
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        orange: colors.orange,
+      },
+    },
+  },
+};
